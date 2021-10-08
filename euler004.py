598bccebd77075f33427be961beb15d4f36bf5b3
provasko <80074452+provasko@users.noreply.github.com>
2021-10-08 16:14:57 +0200

    Update euler004.py
    not working

--- euler004.py
+++ euler004.py
@@ -4,17 +4,32 @@
 
 # First lets made logic for 9009
 n = 9009
-# start from 99 coun
-# for i in range (99, 0, -1):
+# start from 999 countdown two loops until we ll find palindrome
+for i in range(999, 100, -1):
+    for j in range(i, 100, -1):
+        n = i * j
+        a = n // 100000
+        b = (n % 100000) // 10000
+        c = (n % 10000) // 1000
+        d = (n % 1000) // 100
+        e = (n % 100) // 10
+        f = n % 10
+        if a == f and b == e and c == d:
+            break
+            print(n)
+        else:
+            continue
+    break
 
-i = 91
-j = 99
+# hide first logic below
+# i = 91
+# j = 99
 
-a = n // 1000
-b = (n % 1000) // 100
-c = (n % 100) // 10
-d = n % 10
+# a = n // 1000
+# b = (n % 1000) // 100
+# c = (n % 100) // 10
+# d = n % 10
 
-if a == d and b == c:
+# if a == d and b == c:
 
-    print(n)
+#     print(n)

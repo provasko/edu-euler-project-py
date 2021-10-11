7fffe34ccd1899b6b5976a43dc21cd43491f407e
provasko <80074452+provasko@users.noreply.github.com>
2021-10-11 16:11:30 +0200

    Create euler006.py
    Created right logic for numbers conditions

---
+++ euler006.py
@@ -0,0 +1,13 @@
+# The sum of the squares of the first ten natural numbers is 385
+# The square of the sum of the first ten natural numbers is 3025
+# Hence the difference between the sum of the squares of the first ten natural numbers
+# and the square of the sum is 2640.
+# Find the difference between the sum of the squares of the first one hundred natural numbers
+# and the square of the sum.
+
+a = 0
+b = 0
+for i in range(1, 11):
+    a += i * i
+    b += i
+print(b*b - a)

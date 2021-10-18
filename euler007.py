35defa740816672f948d79207b7d96448a683ec6
provasko <80074452+provasko@users.noreply.github.com>
2021-10-18 11:58:02 +0200

    solved

--- euler007.py
+++ euler007.py
@@ -3,15 +3,16 @@
 
 # Create algorithm for 13 (sixth prime number)
 from math import sqrt
+stop_point = 10001
 n = 0
 lst = []
 k = 0
-for i in range(2, 10000):
-    if i > 10:
-        if i % 2 == 0 or i % 10 == 5:
+for i in range(2, 1000000):
+    if (i > 10):
+        if (i % 2 == 0) or (i % 10 == 5):
             continue
     for j in lst:
-        if j > int((sqrt(i)) - 1):
+        if j > int((sqrt(i)) + 1):
             lst.append(i)
             n += 1
             break
@@ -20,5 +21,8 @@ for i in range(2, 10000):
     else:
         lst.append(i)
         n += 1
-
+    if n == stop_point:
+        break
 print(lst[n-1])
+
+# 104743

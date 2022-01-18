fc26cd241fc9146f494fb9f3f1084ea077ac69b5
provasko <80074452+provasko@users.noreply.github.com>
2022-01-18 22:46:24 +0100

    Create euler011.py
    Solved. Actually no maths, just need to find these 4 biggest numbers

---
+++ euler011.py
@@ -0,0 +1,13 @@
+# In the 20×20 grid below, four numbers along a diagonal line have been marked in red.
+# The product of these numbers is 26 × 63 × 78 × 14 = 1788696.
+# What is the greatest product of four adjacent numbers in the same direction(up, down, left, right, or diagonally) in the 20×20 grid?
+
+# Found 4 numbers visually
+x1 = 89
+x2 = 87
+x3 = 94
+x4 = 97
+
+print(x1*x2*x3*x4)
+
+# 70600674

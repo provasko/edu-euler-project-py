605ccc30ef1b4f3aa192cd6a6d262e4562de0fb7
provasko <80074452+provasko@users.noreply.github.com>
2022-01-24 23:49:55 +0100

    Create euler009.py

---
+++ euler009.py
@@ -0,0 +1,12 @@
+# A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+# a^2 + b^2 = c^2
+# For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+# There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+# Find the product abc.
+
+# Euclid Elements
+# a = m * m - n * n, b = 2 * m * n, c = m * m + n * n
+
+# (m * m - n * n) + (2 * m * n) + (m * m + n * n) = 1000
+# 2 * (m * m + m * n) = 1000
+# m * m + m * n = 500

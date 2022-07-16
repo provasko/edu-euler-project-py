549c84a49cefbe0bc5cd11fc36bdecd75d5ce053
provasko <80074452+provasko@users.noreply.github.com>
2022-07-16 11:32:02 +0200

    Update euler004.py

--- euler004.py
+++ euler004.py
@@ -52,3 +52,16 @@ for i in range(9, 1, -1):
 print(result)
 '''
 
+for i in range(900, 999, -1):
+
+    for j in range(i // 2, i, -1):
+        n = i * j
+        a = n // 100000
+        b = (n % 100000) // 10000
+        c = (n % 10000) // 1000
+        d = (n % 1000) // 100
+        e = (n % 100) // 10
+        f = n % 10
+        if a == f and b == e and c == d:
+            break
+            print(n)

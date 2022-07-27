dd81e73cbe9e97a9033f46e231feb32043c3a932
provasko <80074452+provasko@users.noreply.github.com>
2022-07-27 18:34:10 -0400

    Update euler019.py

---
+++ euler019.py
@@ -0,0 +1,21 @@
+'''
+You are given the following information, but you may prefer to do some research for yourself.
+
+1 Jan 1900 was a Monday.
+Thirty days has September,
+April, June and November.
+All the rest have thirty-one,
+Saving February alone,
+Which has twenty-eight, rain or shine.
+And on leap years, twenty-nine.
+A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
+How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+'''
+
+total = 0
+year = 1900
+month = 1
+date = 1
+day = 1
+while year < 2001:
+    while month 

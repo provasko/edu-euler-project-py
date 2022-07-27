dcba8729d1a5d9e1bf5ddf0929ebeb90568dd82f
provasko <80074452+provasko@users.noreply.github.com>
2022-07-26 23:27:42 -1100

    Update euler016.py

--- euler016.py
+++ euler016.py
@@ -2,3 +2,10 @@
 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26. 
 What is the sum of the digits of the number 2^1000?
 '''
+
+total = 0
+a = 2**1000
+
+while a > 0:
+    b = a % 10
+    total += b 

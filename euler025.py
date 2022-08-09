c80439caeef0071b80ba8dba526ca43a878af087
provasko <80074452+provasko@users.noreply.github.com>
2022-08-09 22:25:18 +0200

    Update euler025.py

---
+++ euler025.py
@@ -0,0 +1,3 @@
+# The Fibonacci sequence is defined by the recurrence relation
+# The 12th term, F12, is the first term to contain three digits. 144
+# What is the index of the first term in the Fibonacci sequence to contain 1000 digits?

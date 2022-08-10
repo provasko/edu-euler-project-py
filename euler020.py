ac80d39fcbc3b37b064ea429e5106e38244e1e4f
provasko <80074452+provasko@users.noreply.github.com>
2022-08-10 17:00:52 +0530

    Create euler020.py

---
+++ euler020.py
@@ -0,0 +1,30 @@
+# Factorial digit sum
+
+'''
+n! means n × (n − 1) × ... × 3 × 2 × 1
+
+For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+Find the sum of the digits in the number 100!
+'''
+
+import numbers
+from tkinter import N
+
+
+sum = 0
+facto = 1
+number1 = 100
+while number1 > 0:
+    facto *= number1
+    number1 -= 1
+
+while facto > 0:
+    number2 = facto % 10
+    facto = facto // 10
+    sum += number2
+
+print(sum)
+
+#648

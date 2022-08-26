261e998a8fdad6241a999f1df20e17c2e1c7cf74
provasko <80074452+provasko@users.noreply.github.com>
2022-08-26 11:19:37 +0200

    Update euler018.py

--- euler018.py
+++ euler018.py
@@ -1,5 +1,6 @@
 '''
-By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+By starting at the top of the triangle below and moving to adjacent numbers on the row below, 
+the maximum total from top to bottom is 23.
 
 3
 7 4
